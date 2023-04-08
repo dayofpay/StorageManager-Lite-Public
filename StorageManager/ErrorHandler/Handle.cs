31bfd307a8efd0e1958d7196f91f29e9fb405ea7
Vladislav <42325421+dayofpay@users.noreply.github.com>
2023-04-08 06:20:40 +0300

    Error Handler new feature in progress

--- StorageManager/ErrorHandler/Handle.cs
+++ StorageManager/ErrorHandler/Handle.cs
@@ -4,10 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms.VisualStyles;
+
 namespace StorageManager.ErrorHandler
 {
     class Handle
     {
+        class ErrorCode
+        {
+            public IDictionary<int, string> codeErrors = new Dictionary<int, string>();
+        }
         public static void WriteError(string error)
         {
             if (!Directory.Exists(@"Errors"))
@@ -16,8 +23,21 @@ namespace StorageManager.ErrorHandler
             }
             else
             {
-                File.AppendAllText(@"Errors/error_log.txt", "[" + DateTime.Now.ToString() + "]" + error + Environment.NewLine);
+                File.AppendAllText(@"Errors/error_log.txt", "[" + DateTime.Now.ToString() + "] > " + error + Environment.NewLine);
             }
+        }
+        public static string getErrorInfo(int errorCode)
+        {
+            ErrorCode errorCodes = new ErrorCode();
+            if (errorCodes.codeErrors.ContainsKey(errorCode))
+            {
+                // TODO
+            }
+            else
+            {
+                // TODO
+            }
+            return "";
         }
     }
 }
